4c2e19c222c97051beb22d80613f781a4490abdc
Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>
2014-06-02 22:34:43 -0300

    adding ctr for encryption

--- src/aes_main.c
+++ src/aes_main.c
@@ -5,26 +5,59 @@
 #include "openssl\aes_defs.h"
 #include "openssl\aes_ccms.h"
 
-
 static const uint8_t key[] = { 0x06, 0x17, 0x3B, 0xB8, 0xDE, 0x44, 0xB5, 0xEA, 0xF8, 0x48, 0xF6, 0xAF, 0x6F, 0xD1, 0x0A, 0xA4 };
 static AES_KEY priv_key_enc, priv_key_dec;
 
+static void dump_frame(unsigned char *data, int len)
+{
+    int i, j, k;
+    unsigned char buf[50];
+    for (k = 0; k < len; k += 16)
+    {
+        for (i = k, j = 0; (i< (k + 16)) && (i < len); i++, j += 3)
+            sprintf((char *) &buf[j], "%02X ", data[i]);
+        buf[j] = '\0';
+        printf("%s", buf);
+    }
+}
+
 int main(int argc, char* argv[])
 {
+    
     uint8_t key[] = { 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF };
     uint8_t saddr[] = { 0xAC, 0xDE, 0x48, 0x00, 0x00, 0x00, 0x00, 0x01 };
     uint8_t asn[] = { 0x00, 0x00, 0x00, 0x00, 0x05};
     uint8_t m[] = { 0x61, 0x62, 0x63, 0x64 };
+    uint8_t m2[] = { 0x61, 0x62, 0x63, 0x64 };
     uint8_t lm = 4;
     uint8_t a[] = { 0x69, 0xDC, 0x84, 0x21, 0x43, 0x02, 0x00, 0x00, 0x00, 0x00, 0x48, 0xDE, 0xAC, 0x01, 0x00, 0x00, 0x00, 0x00, 0x48, 0xDE, 0xAC, 0x05};
     uint8_t la = 22;
     uint8_t mac[4];
+    uint8_t mac2[4];
 
-    cbc_mac_enc(a, m, la, lm, saddr, asn, key, mac, 4, AES_CBC_MAC_HW);
-    printf("MAC %02X %02X %02X %02X\n", mac[0], mac[1], mac[2], mac[3]);
+    cbc_mac_enc(a, la, m, lm, saddr, asn, key, mac, 4, AES_CBC_MAC_HW);
+    printf("\nMAC HW:");
+    dump_frame(mac, 5);
+
     mac[0] = mac[1] = mac[2] = mac[3] = 0;
-    cbc_mac_enc(a, m, la, lm, saddr, asn, key, mac, 4, AES_CBC_MAC_FW);
-    printf("MAC %02X %02X %02X %02X\n", mac[0], mac[1], mac[2], mac[3]);
+    cbc_mac_enc(a, la, m, lm, saddr, asn, key, mac, 4, AES_CBC_MAC_FW);
+    printf("\nMAC FW:");
+    dump_frame(mac, 5);
+    memcpy(mac2, mac, 4);
+
+    aes_ctr_enc(m, lm, saddr, asn, key, mac, 4, AES_CTR_HW);
+
+    printf("\nE. PAYLOAD HW: ");
+    dump_frame(m, lm);
+    printf("\nE. MAC HW    : "); 
+    dump_frame(mac, 4);
+
+    aes_ctr_enc(m2, lm, saddr, asn, key, mac2, 4, AES_CTR_FW);
+
+    printf("\nE. PAYLOAD FW: ");
+    dump_frame(m2, lm);
+    printf("\nE. MAC FW    : ");
+    dump_frame(mac2, 4);
 
     return 0;
 }
--- src/openssl/aes_ccms.c
+++ src/openssl/aes_ccms.c
@@ -23,8 +23,8 @@ int cbc_mac_enc_fw(uint8_t *buffer, uint8_t len, AES_KEY *key_enc)
 }
 
 int cbc_mac_enc(uint8_t *a, 
-                uint8_t *m,
                 uint8_t len_a,
+                uint8_t *m,
                 uint8_t len_m,
                 uint8_t saddr[8],
                 uint8_t asn[5],
@@ -93,6 +93,107 @@ int cbc_mac_enc(uint8_t *a,
         cbc_mac_enc_fw(buffer,len,&key_enc);
 
     memcpy(mac, &buffer[len - 16], len_mac);
+
+    return 0;
+}
+
+// openssl
+static void inc_counter(unsigned char *counter) 
+{
+    u32 n = 16;
+    u8  c;
+    do {
+        --n;
+        c = counter[n];
+        ++c;
+        counter[n] = c;
+        if (c) return;
+    } while (n);
+}
+
+int aes_ctr_enc_fw(uint8_t *buffer, uint8_t len, AES_KEY *key_enc, uint8_t iv[16])
+{
+    uint8_t n, k, nb, *pbuf;
+    uint8_t eiv[16];
+
+    nb = len >> 4;
+    for (n = 0; n < nb; n++) {
+        pbuf = &buffer[16 * n];
+        AES_ecb_encrypt(iv, eiv, key_enc, AES_ENCRYPT);
+        // may be faster if vector are aligned to 4 bytes (use long instead char in xor)
+        for (k = 0; k < 16; k++){
+           pbuf[k] ^= eiv[k];
+        }
+        inc_counter(iv);
+    }
+
+    return 0;
+
+}
+
+int aes_ctr_enc(uint8_t *m,
+    uint8_t len_m,
+    uint8_t saddr[8],
+    uint8_t asn[5],
+    uint8_t *key,
+    uint8_t *mac,
+    uint8_t len_mac,
+    uint8_t aes_ctr_support)
+{
+    uint8_t pad_len;
+    uint8_t len;
+    uint8_t iv[16];
+    uint8_t ebuf[16];
+    uint32_t num ;
+    uint8_t buffer[128 + 16]; // max buffer plus mac
+    AES_KEY key_enc;
+
+    // asserts here
+    if (!((len_mac == 4) || (len_mac == 8) || (len_mac == 16)))
+        return -1;
+
+    if (len_m > 127)
+        return -2;
+
+    if (mac == 0)
+        return -3;
+
+    if (!((aes_ctr_support == AES_CTR_HW) || (aes_ctr_support == AES_CTR_FW)))
+        return -4;
+
+    num = 0;
+    memset(ebuf, 0, 16);
+
+    // iv (flag (1B) | source addr (8B) | ASN (5B) | cnt (2B)
+    iv[0] = 0x01;
+    memcpy(&iv[1], saddr, 8);
+    memcpy(&iv[9], asn, 5); // assign byte by byte or copy ?
+    iv[14] = 0x00;
+    iv[15] = 0x00;
+
+    // first block is mac
+    memcpy(buffer, mac, len_mac);
+    memset(&buffer[len_mac], 0, 16 - len_mac);
+    len = 16;
+
+    //  (((x >> 4) + 1)<<4) - x   or    16 - (x % 16) ?
+    // m + padding
+    pad_len = (((len_m >> 4) + 1) << 4) - len_m;
+    pad_len = pad_len == 16 ? 0 : pad_len;
+    memcpy(&buffer[len], m, len_m);
+    len += len_m;
+    memset(&buffer[len], 0, pad_len);
+    len += pad_len;
+
+    AES_set_encrypt_key(key, 128, &key_enc);
+
+    if (aes_ctr_support == AES_CTR_HW)
+        AES_ctr128_encrypt(buffer, buffer, len, &key_enc, iv, ebuf, &num);
+    else
+        aes_ctr_enc_fw(buffer, len, &key_enc, iv);
+
+    memcpy(m, &buffer[16], len_m);
+    memcpy(mac, buffer, len_mac);
 
     return 0;
 }
